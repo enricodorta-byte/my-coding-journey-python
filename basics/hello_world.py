2619e97ea85460fd1329dc0ded3b7670b8a1cab9
Enrico Dorta <enricodorta@gmail.com>
2026-01-04 22:36:16 +0100

    Add basics/hello_world.py - My first personal Python script

---
+++ basics/hello_world.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print("I'm Enrico Dorta, 34 years old, and I love programming!")
+print("former retail manager, now on the path to becoming an AI Engineer.")
+print("Started: January 03 2026")
+print("Target: USI Lugano - Master in Artificial Intelligence")
+
+name = input("\nWhat's your name? ")
+print(f"\nNice to meet you, {name}! Let's buid the future toghether, one commit at a time 🚀")
